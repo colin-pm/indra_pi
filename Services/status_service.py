109148be86398f85185f19420cc0730d702e67a5
Colin McAllister <colinmca@iastate.edu>
2018-05-30 19:48:26 -0500

    Moved over reusable code to status service

--- Services/status_service.py
+++ Services/status_service.py
@@ -18,46 +18,6 @@ MQTT_KEEPALIVE = 60
 valve = ''
 mqttc = None
 
-def watering_loop():
-    global valve
-    while True:
-        # Get day of week
-        day = get_day_num()
-        # Get waterings for current day
-        waterings = get_waterings(day)
-        # Get current time for watering
-        curr_time = time.strftime('%H:%M').spit(':')
-        # Check watering schedule and open or close valve if appropriate
-        for watering in waterings:
-            stop_time = (watering[0] + int(watering[2] / 60), watering[1] + (watering[2] % 60))
-            if (curr_time[0] == watering[0] and curr_time[1] > watering[1]) or (curr_time[0] > watering[0]):
-                if (curr_time[0] == stop_time[0] and curr_time[1] < stop_time[1]) or (curr_time[0] < stop_time[0]):
-                    if valve == "closed":
-                        open_valve()
-                else:
-                    if valve == "open":
-                        close_valve()
-            else:
-                if valve == "open":
-                    close_valve()
-        # Sleep for a minute before checking again
-        time.sleep(60)
-
-
-
-def open_valve():
-    cmd_payload = {'valve': 'open'}
-    mqttc.publish('indra/command/valve', json.dumps(cmd_payload), qos=2)
-
-
-def close_valve():
-    cmd_payload = {'valve': 'closed'}
-    mqttc.publish('indra/command/valve', json.dumps(cmd_payload), qos=2)
-
-
-def received_valve_msg(client, userdata, message):
-    global valve
-    valve = json.loads(message.payload)['valve']
 
 
 def init_mqtt():
@@ -70,17 +30,6 @@ def init_mqtt():
     # Register callback for messages when valve state has changed
     mqttc.message_callback_add('indra/device/valve', received_valve_msg)
     mqttc.loop_start()
-
-
-def get_waterings(day):
-    command = 'Select hour, minute, duration FROM waterings WHERE day == ? ORDER BY hour, minute'
-    db = sqlite3.connect(DATABASE)
-    return [row for row in db.execute(command, day)]
-
-
-def get_day_num():
-    day_num = date.isoweekday(date.today())
-    return day_num + 1 if day_num != 7 else 1
 
 
 if __name__ == "__main__":
